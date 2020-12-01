0bfa4f56ce46e06789361888b4c81adb71d32f08
Mazin <ahmedmaazin@gmail.com>
2020-12-01 15:40:21 +0800

    chore: updates

--- example/src/App.vue
+++ example/src/App.vue
@@ -9,8 +9,9 @@
         <br>
         <lottie-vue-player :src="`https://assets10.lottiefiles.com/packages/lf20_tzjfwgud.json`"
                            :theme="options.theme"
-                           :player-size="options.playerSize" style="width: 100%; height:500px"
-                           :player-controls="true"></lottie-vue-player>
+                           :player-size="options.playerSize"
+                           :player-controls="true"
+                           style="width: 100%; height:400px"></lottie-vue-player>
     </div>
 </template>
 
