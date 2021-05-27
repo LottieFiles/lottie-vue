4ae9f6fd101aaa10da2dfd23bf9d752ab9a71293
karamalie <karam7898@gmail.com>
2021-05-27 21:38:04 +0800

    feat(bundling): added bundling scripts and processes

--- example/src/main.js
+++ example/src/main.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import LottieVuePlayerPlugin from "../../src";
+import Vue from "vue";
+import App from "./App.vue";
+import LottieVuePlayer from "@lottiefiles/vue-lottie-player";
 
-Vue.use(LottieVuePlayerPlugin)
+Vue.use(LottieVuePlayer);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-    render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
