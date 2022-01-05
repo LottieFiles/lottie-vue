02d76b69f6db713c6b44eba64c58372816587604
karamalie <karam7898@gmail.com>
2022-01-05 21:49:06 +0800

    fix(controls css bundling issue): fixed controls css bundling issue

--- vue.config.js
+++ vue.config.js
@@ -2,24 +2,22 @@
 const path = require(`path`);
 
 module.exports = {
-    configureWebpack: {
-
-    },
-    chainWebpack: config => {
-        config.module
-            .rule('vue')
-            .use('vue-loader')
-            .tap(options => {
-                return {
-                    ...options,
-                    compilerOptions: {
-                        isCustomElement: tag => tag === 'lottie-player'
-                    }
-                }
-            })
-    }
-}
-
+  configureWebpack: {},
+  chainWebpack: (config) => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .tap((options) => {
+        return {
+          ...options,
+          compilerOptions: {
+            isCustomElement: (tag) => tag === "lottie-player",
+          },
+        };
+      });
+  },
+  css: { extract: false },
+};
 
 // module.exports = {
 //     configureWebpack: {
